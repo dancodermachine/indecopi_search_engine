01a1614f0c828b64ba75d295dd47306d733d147f
Daniel Villanueva <60199577+dancodermachine@users.noreply.github.com>
2022-02-14 23:13:32 +0000

    change nltk.download

--- indesearch.py
+++ indesearch.py
@@ -17,7 +17,7 @@ __email__ = '2144810@brunel.ac.uk'
 __website__ = 'https://www.linkedin.com/in/danielvillanuevanunez/'
 __copyright__ = 'Copyright 2022, Daniel Villanueva'
 
-nltk.download('punkt')
+nltk.download('all')
 
 # List of stopwords in Spanish.
 stopword_es = nltk.corpus.stopwords.words('spanish')
